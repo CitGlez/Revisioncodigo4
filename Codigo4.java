7fdf061bd1c7cf58bb47efa44909d60b89a18eec
CitGlez <140109396+CitGlez@users.noreply.github.com>
2023-09-08 11:11:32 -0600

    Add files via upload

---
+++ Codigo4.java
@@ -0,0 +1,54 @@
+//El codigo siguiente funciona con if e if else, son dos turnos, cada jugador elije su piedra/papel/tijera y cuando el jugador siguiente elije se define cual de los dos gano; con el equals se definen los valores del resultado
+
+//se importa el scanner
+import java.util.Scanner;
+
+//corregir el nombre del archivo
+public class Codigo4 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        // Solicitar la entrada del jugador 1
+        System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+        String j1 = s.nextLine();
+
+        // Solicitar la entrada del jugador 2 y corregir el Scanner
+        System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+        String j2 = s.nextLine();
+
+        // Corregir la comparación de cadenas usando equals
+        if (j1.equals(j2)) {
+            System.out.println("Empate");
+        } else {
+            int g = 2;
+            switch (j1) {
+                case "piedra":
+                    if (j2.equals("tijeras")) {
+                        g = 1;
+                    }
+                    break;
+
+                case "papel":
+                    if (j2.equals("piedra")) {
+                        g = 1;
+                    }
+                    break;
+
+                case "tijeras":
+                    if (j2.equals("papel")) {
+                        g = 1;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            // Mostrar quién gana
+            System.out.println("Gana el jugador " + g);
+        }
+
+        // Cerrar el Scanner
+        s.close();
+    }
+}
